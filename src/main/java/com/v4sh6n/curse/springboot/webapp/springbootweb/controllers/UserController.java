f600d10d76d10de313823588585449e2ad7b9810
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2024-05-08 18:29:05 +0200

    update controllers & details.html

--- src/main/java/com/v4sh6n/curse/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/v4sh6n/curse/springboot/webapp/springbootweb/controllers/UserController.java
@@ -4,16 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.v4sh6n.curse.springboot.webapp.springbootweb.models.User;
+
 @Controller
 public class UserController {
 
     //metodo handler procesa request y devuelve response bien sea vista o html
     @GetMapping("/details")
     public String details(Model model) {
+        User user = new User("John","Doe");
+
         model.addAttribute("title", "Hello World SprintBoot from GIT");
-        model.addAttribute("name", "John");
-        model.addAttribute("lastName", "Doe");
-    
+        model.addAttribute("user",user);
+          
     
         
         return "details";
--- src/main/java/com/v4sh6n/curse/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/v4sh6n/curse/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -4,22 +4,28 @@ package com.v4sh6n.curse.springboot.webapp.springbootweb.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.v4sh6n.curse.springboot.webapp.springbootweb.models.User;
+
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
     //metodo handler procesa request y devuelve response bien sea vista o html
-    @GetMapping("/details2")
+    //para mapear un request a una ruta url
+    //@RequestMapping(path="/details", method = RequestMethod.GET) 
+    @GetMapping("/details")
     public Map<String, Object> details() {
-       
+        User user = new User("John","Doe");
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hello World SprintBoot from GIT");
-        body.put("name", "John");
-        body.put("lastName", "Doe");
+        body.put("user",user);
+
     
     
         
